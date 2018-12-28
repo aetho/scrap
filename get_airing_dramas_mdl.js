50c91fb584e2076c29353493ee26bbaab7630988
Phuoc Nguyen <chewinggum1808@gmail.com>
2018-12-28 11:42:10 +1000

    Implemented get airing URLs from MDL

--- get_airing_dramas_mdl.js
+++ get_airing_dramas_mdl.js
@@ -3,6 +3,7 @@
 const puppeteer = require('puppeteer');
 
 const url = 'https://wiki.d-addicts.com/List_of_Dramas_aired_in_Korea_by_Network_in_2018';
+const reYear = 2018;
 
 (async () => {
 	const browser = await puppeteer.launch();
@@ -13,11 +14,16 @@ const url = 'https://wiki.d-addicts.com/List_of_Dramas_aired_in_Korea_by_Network
 	await page.waitForSelector('.mw-parser-output'); // Make dramas have loaded before continuing
 	const airing = await page.$$('#airing > td:nth-child(3) > a:nth-child(1)'); // Get drama anchors elements
 
+
 	// extract href attribute from anchors
 	for (let i = 0; i < airing.length; i++) {
 		let dramaURL = await (await airing[i].getProperty('href')).jsonValue();
+
+		dramaURL = dramaURL.replace('https://wiki.d-addicts.com/', 'https://mydramalist.com/search?q=');
+		dramaURL += `&adv=titles&re=${reYear},${reYear}&adv=titles&ty=68&co=3&re=2018,2018&so=relevance`;
+		dramaURL = dramaURL.replace(new RegExp('_', 'g'), '+');
+		
 		console.log(dramaURL);
 	}
-	
 	await browser.close();
 })();
